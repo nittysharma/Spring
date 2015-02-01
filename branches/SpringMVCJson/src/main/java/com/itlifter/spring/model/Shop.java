ef547ed8a9b3bcd1de895aab64714d222c76b524
nittysharma <gaccount4m@gmail.com>
2015-02-01 08:48:37 +0530

    first commit for spring
    http://localhost:8080/SpringMVCJson/persons

---
+++ branches/SpringMVCJson/src/main/java/com/itlifter/spring/model/Shop.java
@@ -0,0 +1,21 @@
+package com.itlifter.spring.model;
+
+public class Shop {
+	 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String[] staffName) {
+		this.staffName = staffName;
+	}
+	String name;
+	String staffName[];
+ 
+ 
+}
